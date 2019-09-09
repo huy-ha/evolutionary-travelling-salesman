a3acce5e7e91cc6901fe80cfb96f7476cdd89c5f
Huy Ha <hqh2101@columbia.edu>
2019-09-09 17:48:16 -0400

    coded outline for TravellingSalesman in C#

---
+++ hw1/EvolutionaryTravellingSalesman/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace EvolutionaryTravellingSalesman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO do file read
+            var filePath = "";
+            var cities = File.ReadAllLines(filePath)
+                .Select(line => new TravellingSalesman.City(0, 0));
+
+            int populationCount = 10000;
+            var salesman = new TravellingSalesman(cities);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ hw1/EvolutionaryTravellingSalesman/TravellingSalesman.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TravellingSalesman
+{
+    public float Cost
+    {
+        get;
+        protected set;
+    }
+
+    private List<City> m_path;
+
+	public TravellingSalesman(IEnumerable<City> cities)
+	{
+        m_path = FindRandomPath(cities);
+        CalculateCost();
+	}
+
+    private List<City> FindRandomPath(IEnumerable<City> cities)
+    {
+        var rand = new Random();
+        return new List<City>(cities
+            .Zip(
+            cities.Select(city => rand.NextDouble()), //create random order
+            (city, order) => new {City=city,Order=order}) //create object with order and city
+            .OrderBy(obj => obj.Order) //sort by order
+            .Select(obj => obj.City)); //get just the city
+    }
+
+    private void CalculateCost()
+    {
+        Cost = 0;
+        for(int i = 0; i < m_path.Count - 1; i++)
+        {
+            Cost += City.Distance(m_path[i], m_path[i + 1]);
+        }
+    }
+
+    public class City
+    {
+        private float m_x = 0;
+        private float m_y = 0;
+
+        public City(float x, float y) {
+            m_x = x;
+            m_y = y;
+        }
+
+        public static float Distance(City city1,City city2)
+        {
+            float dx = city1.m_x - city2.m_x;
+            float dy = city1.m_y - city2.m_y;
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}

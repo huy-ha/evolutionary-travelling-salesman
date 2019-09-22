6217154162287dfa421748969732a11bae66e6de
huy-ha <hqh2101@columbia.edu>
2019-09-22 15:59:40 -0400

    implemented simulated annealing and elitist group

--- hw1/EvolutionaryTravellingSalesman/Program.cs
+++ hw1/EvolutionaryTravellingSalesman/Program.cs
@@ -17,29 +17,37 @@ namespace EvolutionaryTravellingSalesman
         {
             var filePath = "tsp.txt";
             var cities = File.ReadAllLines(filePath)
-                .Select(line => {
-                    var coors = line.Split("\t",2);
+                .Select(line =>
+                {
+                    var coors = line.Split("\t", 2);
                     var x = float.Parse(coors[0]);
                     var y = float.Parse(coors[1]);
                     return new TravellingSalesman.City(x, y);
                 });
 
             // Initialize population
-            int populationCount = 500;
+            int populationCount = 600;
             var population = new LinkedList<TravellingSalesman>();
-            for(int i = 0; i < populationCount; i++)
+            for (int i = 0; i < populationCount; i++)
             {
                 population.AddLast(new TravellingSalesman(cities));
             }
             // Start evolution
-            int generationCount = 100000;
+            int generationCount = 100;
             bool findShortestPath = true;
-            float elitistPercentage = 0.01f;
+            float elitistPercentage = 0.02f;
             var selector = new SimulatedAnnealingSelected();
-            float mutationFactor = 0.6f;
-            float mutationFactorDecay = 0.9999f;
+            float initMutationFactor = 0.5f;
+            float mutationFactor = initMutationFactor;
+            float mutationFactorDecay = 1f;
 
-            int saveInterval = generationCount/50;
+            // Simulated Annealing
+            float init_T = 1f;
+            float T = init_T; //Probability of choosing a worse gene anyways
+            float T_decay = 0.9999f;
+
+            int logCount = 50;
+            int logFrequency = generationCount / logCount;
 
             // Timer for optimization purposes
             selector.Reset();
@@ -51,46 +59,52 @@ namespace EvolutionaryTravellingSalesman
             List<float> minCosts = new List<float>(generationCount);
             List<float> maxCosts = new List<float>(generationCount);
             List<float> avgCosts = new List<float>(generationCount);
-            for(int generation = 0; generation < generationCount; generation++){
-                #if DEBUG
-                    Console.WriteLine("\nGeneration " + generation);
-                #endif
+            for (int generation = 0; generation < generationCount; generation++)
+            {
+#if DEBUG
+                Console.WriteLine("\nGeneration " + generation);
+#endif
                 //select
-                // (var elites,var parents) = selector.Select(population,elitistPercentage,findShortestPath);
-                var parents = selector.Select(population,elitistPercentage,findShortestPath);
+                var parents = selector.Select(population, elitistPercentage, findShortestPath);
+#if DEBUG
                 float parentsAvgDist = parents.Average(salesman => salesman.Cost);
                 float parentsMaxDist = parents.Max(salesman => salesman.Cost);
                 float parentsMinDist = parents.Min(salesman => salesman.Cost);
-                #if DEBUG
-                    Console.WriteLine("Average Parent: " + parentsAvgDist + ", Max: " + parentsMaxDist + ", Min: "+ parentsMinDist);
-                #endif
+                Console.WriteLine("Average Parent: " + parentsAvgDist + ", Max: " + parentsMaxDist + ", Min: " + parentsMinDist);
+#endif
                 //mutate
-                var offspring = parents.Select(parent => new TravellingSalesman(parent,mutationFactor));
-                while(offspring.Count() < populationCount){
+                var offspring = parents.Select(parent => new TravellingSalesman(parent, mutationFactor));
+                while (offspring.Count() < populationCount)
+                {
                     offspring = offspring.Concat(                             // 4. Concatenate newly created children with previous children
                         await Task.WhenAll(                                   // 3. Wait until all constructions of children is finished
-                            parents.Select(                                                       
+                            parents.Select(
                                 parent => Task.Run(() =>                      // 2. Run the constructor asynchronously 
-                                new TravellingSalesman(parent))).ToArray())); // 1. From each parent, create a new TravellingSalesman
+                                new TravellingSalesman(parent, T))).ToArray())); // 1. From each parent, create a new TravellingSalesman
                 }
+                var elites = population.OrderBy(salesman => salesman.Cost).Take((int)(elitistPercentage * populationCount));
                 population = new LinkedList<TravellingSalesman>(offspring.Take(populationCount));
+                population = new LinkedList<TravellingSalesman>(elites.Concat(offspring).OrderBy(salesman => salesman.Cost).Take(populationCount));
+                T *= T_decay;
                 mutationFactor *= mutationFactorDecay;
 
                 // Collect Stats
                 float avgDist = population.Average(salesman => salesman.Cost);
-                var maxSalesman = population.Aggregate((salesman1,salesman2)=> salesman1.Cost > salesman2.Cost ? salesman1 : salesman2);
-                var minSalesman = population.Aggregate((salesman1,salesman2)=> salesman1.Cost < salesman2.Cost ? salesman1 : salesman2);
+                var maxSalesman = population.Aggregate((salesman1, salesman2) => salesman1.Cost > salesman2.Cost ? salesman1 : salesman2);
+                var minSalesman = population.Aggregate((salesman1, salesman2) => salesman1.Cost < salesman2.Cost ? salesman1 : salesman2);
                 float maxDist = maxSalesman.Cost;
                 float minDist = minSalesman.Cost;
+
                 minCosts.Add(minDist);
                 maxCosts.Add(maxDist);
                 avgCosts.Add(avgDist);
-                #if DEBUG
-                    Console.WriteLine("Average: " + avgDist + ", Max: " + maxDist + ", Min: "+ minDist);
-                    Console.WriteLine("Population:" + population.Count + ", MutationFactor:" + mutationFactor);
-                #endif
-                if(mutationFactor == 0) break;
-                if(generation%saveInterval == 0){
+#if DEBUG
+                Console.WriteLine("Average: " + avgDist + ", Max: " + maxDist + ", Min: " + minDist);
+                Console.WriteLine("Population:" + population.Count + ", MutationFactor:" + mutationFactor);
+#endif
+                if (mutationFactor == 0) break;
+                if (generation % logFrequency == 0 || generation == generationCount - 1)
+                {
                     Console.WriteLine("Saving Generation " + generation);
                     bestSalesmanOutput += "Generation " + generation + "\n";
                     bestSalesmanOutput += minSalesman.PrintPath() + "\n";
@@ -100,10 +114,24 @@ namespace EvolutionaryTravellingSalesman
             TimeSpan ts = stopWatch.Elapsed;
             Console.WriteLine(ts.TotalSeconds);
             // Save Output
+            string config = "";
+            config += "Cities File: " + filePath + "\n";
+            config += "FindShortestPath: " + findShortestPath + "\n";
+            config += "Generation Count: " + generationCount + "\n";
+            config += "\n";
+            config += "Population Count: " + populationCount + "\n";
+            config += "Elitist Percentage: " + elitistPercentage + "\n";
+            config += "\n";
+            config += "Initial Mutation Factor: " + initMutationFactor + "\n";
+            config += "Mutation Factor Decay: " + mutationFactorDecay + "\n";
+            config += "\n";
+            config += "Init T: " + init_T + "\n";
+            config += "T Decay: " + T_decay + "\n";
+            System.IO.File.WriteAllText("Config.txt", config);
             System.IO.File.WriteAllText("BestSalesMan.txt", bestSalesmanOutput);
-            System.IO.File.WriteAllText("MaxCosts.txt", string.Join("\n",maxCosts));
-            System.IO.File.WriteAllText("MinCosts.txt", string.Join("\n",minCosts));
-            System.IO.File.WriteAllText("AvgCosts.txt", string.Join("\n",avgCosts));
+            System.IO.File.WriteAllText("MaxCosts.txt", string.Join("\n", maxCosts));
+            System.IO.File.WriteAllText("MinCosts.txt", string.Join("\n", minCosts));
+            System.IO.File.WriteAllText("AvgCosts.txt", string.Join("\n", avgCosts));
         }
     }
 }
--- hw1/EvolutionaryTravellingSalesman/TravellingSalesman.cs
+++ hw1/EvolutionaryTravellingSalesman/TravellingSalesman.cs
@@ -5,42 +5,64 @@ using System.Linq;
 public class TravellingSalesman
 {
     #region Variables
+    private Random m_rand;
     public float Cost
     {
         get;
         protected set;
     }
 
-    public float Fitness {
-        get {
-            return 1000/Cost;
+    public float Fitness
+    {
+        get
+        {
+            return 1000 / Cost;
         }
     }
 
     private List<City> m_path;
     #endregion
 
-	public TravellingSalesman(IEnumerable<City> cities)
-	{
+    public TravellingSalesman(IEnumerable<City> cities)
+    {
         m_path = FindRandomPath(cities);
         CalculateCost();
-	}
+        m_rand = new Random();
+    }
 
-    public TravellingSalesman(TravellingSalesman parent,float maxMutationFactor=0.3f){
+    public TravellingSalesman(TravellingSalesman parent, float T, float maxMutationFactor = 0.3f)
+    {
         m_path = new List<City>(parent.m_path);
-        int count = m_path.Count(); 
-        int maxSwaps = Math.Max((int) (count*maxMutationFactor),1);        
-        var rand = new Random();
-        int swaps = rand.Next() % maxSwaps;
-        for(int i = 0; i < swaps;i++){
-            int idx1 = rand.Next()%count;
-            int idx2 = rand.Next()%count;
-            //swap two cities
-            var tmp = m_path[idx1];
-            m_path[idx1] = m_path[idx2];
-            m_path[idx2] = tmp;
+        m_rand = new Random();
+        Cost = parent.Cost;
+        int count = m_path.Count();
+        int maxMutations = Math.Max((int)(count * maxMutationFactor), 1);
+        int mutations = m_rand.Next() % maxMutations;
+        for (int i = 0; i < mutations; i++)
+        {
+            Mutate(T, count);
         }
         CalculateCost();
+    }
+
+    private void Mutate(float T, int count)
+    {
+        var testPath = new List<City>(m_path);
+        Swap(testPath, m_rand.Next() % count, m_rand.Next() % count);
+        float testPathCost = CalculateCost(testPath);
+        if (testPathCost < Cost || m_rand.NextDouble() < T)
+        {
+            m_path = testPath;
+            Cost = testPathCost;
+        }
+
+    }
+
+    private void Swap(List<City> path, int i1, int i2)
+    {
+        var tmp = path[i1];
+        path[i1] = path[i2];
+        path[i2] = tmp;
     }
 
     private List<City> FindRandomPath(IEnumerable<City> cities)
@@ -49,26 +71,35 @@ public class TravellingSalesman
         return new List<City>(cities
             .Zip(
             cities.Select(city => rand.NextDouble()), //create random order
-            (city, order) => new {City=city,Order=order}) //create object with order and city
+            (city, order) => new { City = city, Order = order }) //create object with order and city
             .OrderBy(obj => obj.Order) //sort by order
             .Select(obj => obj.City)); //get just the city
     }
 
-    private void CalculateCost()
+    private float CalculateCost(List<City> path)
     {
-        Cost = 0;
-        for(int i = 0; i < m_path.Count - 1; i++)
+        float cost = 0;
+        for (int i = 0; i < path.Count - 1; i++)
         {
-            Cost += City.Distance(m_path[i], m_path[i + 1]);
+            cost += City.Distance(path[i], path[i + 1]);
         }
+        return cost;
     }
 
-    public override string ToString(){
-        return ""+Fitness;
+    private float CalculateCost()
+    {
+        Cost = CalculateCost(m_path);
+        return Cost;
     }
 
-    public string PrintPath(){
-        return string.Join("|",m_path);
+    public override string ToString()
+    {
+        return "" + Fitness;
+    }
+
+    public string PrintPath()
+    {
+        return string.Join("|", m_path);
     }
 
     public class City
@@ -76,19 +107,21 @@ public class TravellingSalesman
         private float m_x = 0;
         private float m_y = 0;
 
-        public City(float x, float y) {
+        public City(float x, float y)
+        {
             m_x = x;
             m_y = y;
         }
 
-        public static float Distance(City city1,City city2)
+        public static float Distance(City city1, City city2)
         {
             float dx = city1.m_x - city2.m_x;
             float dy = city1.m_y - city2.m_y;
             return MathF.Sqrt(dx * dx + dy * dy);
         }
 
-        public override string ToString(){
+        public override string ToString()
+        {
             return m_x + " " + m_y;
         }
     }

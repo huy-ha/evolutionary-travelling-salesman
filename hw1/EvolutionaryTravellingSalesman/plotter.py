6719a701ec2ae7df718d3c6f9c5b395225aa9396
huy-ha <hqh2101@columbia.edu>
2019-09-23 10:09:23 -0400

    find files in output

--- hw1/EvolutionaryTravellingSalesman/plotter.py
+++ hw1/EvolutionaryTravellingSalesman/plotter.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
-import re 
-def read_file(filename):
-    return [float(x) for x in open(filename,"r").readlines()]
+import re
 
-def read_path(filename,generation):
-    file = open(filename,"r")
+
+def read_file(filename):
+    return [float(x) for x in open(filename, "r").readlines()]
+
+
+def read_path(filename, generation):
+    file = open(filename, "r")
     while True:
         curr = int(re.findall('\d+', file.readline())[0])
         if curr >= generation:
@@ -14,30 +17,32 @@ def read_path(filename,generation):
     y = []
     for city in cities:
         x.append(float(city[0]))
-        y.append(float(city[1])) 
-    return x,y
+        y.append(float(city[1]))
+    return x, y
+
 
 def plot_costs():
-    maxCosts = read_file("MaxCosts.txt")
-    minCosts = read_file("MinCosts.txt")
-    avgCosts = read_file("AvgCosts.txt") 
+    maxCosts = read_file("output/MaxCosts.txt")
+    minCosts = read_file("output/MinCosts.txt")
+    avgCosts = read_file("output/AvgCosts.txt")
 
-    plt.plot(maxCosts,label="Max Costs")
-    plt.plot(minCosts,label="Min Costs")
-    plt.plot(avgCosts,label="Average Costs")
+    plt.plot(maxCosts, label="Max Costs")
+    plt.plot(minCosts, label="Min Costs")
+    plt.plot(avgCosts, label="Average Costs")
     plt.legend()
     plt.title("Genetic Travelling Salesman")
     plt.ylabel('Costs')
     plt.xlabel('Generation')
     plt.show()
 
+
 def plot_path(generation=0):
-    x,y = read_path("BestSalesMan.txt",generation)
-    plt.plot(x,y)
+    x, y = read_path("output/BestSalesMan.txt", generation)
+    plt.plot(x, y)
     plt.title("Genetic Travelling Salesman Path, generation {}".format(generation))
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_costs();
-    # plot_path(generation=98000)
+    # plot_costs()
+    plot_path(generation=49999)

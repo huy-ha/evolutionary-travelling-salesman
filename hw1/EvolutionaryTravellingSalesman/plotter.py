fixed wrong dimensions error

hw1/EvolutionaryTravellingSalesman/plotter.py
@@ -68,27 +68,46 @@ def average_runs(runs):
     minCosts = []
     minCostsErr = []
     avgCosts = []
-
+    counts = []
     for run in runs:
         runEvals = []
         runMaxCosts = []
         runMinCosts = []
         runAvgCosts = []
+        count = 0
         for i in run:
             dirname = get_dir_name(int(i))
-            runEvals.append(read_file("{}/Evaluations.txt".format(dirname)))
-            runMaxCosts.append(read_file("{}/MaxCosts.txt".format(dirname)))
-            runMinCosts.append(read_file("{}/MinCosts.txt".format(dirname)))
-            runAvgCosts.append(read_file("{}/AvgCosts.txt".format(dirname)))
-        maxCosts.append(np.mean(runMaxCosts, axis=0))
+            new_eval_list = read_file("{}/Evaluations.txt".format(dirname))
+            if len(runEvals) == 0:
+                runEvals.append(new_eval_list)
+                count = len(new_eval_list)
+            else:
+                if len(runEvals[0]) != len(new_eval_list):
+                    print("Mismatch length of {} and {}. Taking {}...".format(
+                        count, len(new_eval_list), min(count, len(new_eval_list))))
+                count = min(count, len(new_eval_list))
+                evals.append(new_eval_list)
+            runMaxCosts.append(
+                read_file("{}/MaxCosts.txt".format(dirname)))
+            runMinCosts.append(
+                read_file("{}/MinCosts.txt".format(dirname)))
+            runAvgCosts.append(
+                read_file("{}/AvgCosts.txt".format(dirname)))
+        runEvals = [runEval[0:count] for runEval in runEvals]
+        runMaxCosts = [runMaxCost[0:count] for runMaxCost in runMaxCosts]
+        runMinCosts = [runMinCost[0:count] for runMinCost in runMinCosts]
+        runAvgCosts = [runAvgCost[0:count] for runAvgCost in runAvgCosts]
+        n_sqrt = math.sqrt(len(i))
+        maxCosts.append(np.mean(runMaxCosts, axis=0)[0:count])
         maxCostsErr.append(
-            np.std(runMaxCosts, axis=0) / math.sqrt(len(i)))
-        minCosts.append(np.mean(runMinCosts, axis=0))
+            np.std(runMaxCosts, axis=0) / n_sqrt)
+        minCosts.append(np.mean(runMinCosts, axis=0)[0:count])
         minCostsErr.append(
-            np.std(runMinCosts, axis=0) / math.sqrt(len(i)))
-        avgCosts.append(np.mean(runAvgCosts, axis=0))
-        evals.append(np.mean(runEvals, axis=0))
-    return evals, maxCosts, minCosts, avgCosts, maxCostsErr, minCostsErr
+            np.std(runMinCosts, axis=0) / n_sqrt)
+        avgCosts.append(np.mean(runAvgCosts, axis=0)[0:count])
+        evals.append(np.mean(runEvals, axis=0)[0:count])
+        counts.append(count)
+    return counts, evals, maxCosts, minCosts, avgCosts, maxCostsErr, minCostsErr
 
 
 def plot_learning_curve(args):
@@ -100,17 +119,18 @@ def plot_learning_curve(args):
     colors = ['red', 'lime', 'cyan', 'blue', 'yellow', 'orange']
     if title is None:
         title = "Genetic Travelling Salesman"
-    evals, maxCost, minCost, avgCost, maxCostErr, minCostErr = average_runs(
+    counts, evals, maxCost, minCost, avgCost, maxCostErr, minCostErr = average_runs(
         runs)
-    for i in range(len(labels)):
+    for i in range(len(runs)):
+        count = counts[i]
         if logconfig is not None and logconfig == "y":
             evals[i] = [math.log(x) for x in evals[i]]
         if config == "max":
-            plt.errorbar(evals[i], maxCost[i], yerr=maxCostErr[i],
-                         label=labels[i], color=colors[i], ecolor=colors[i], errorevery=1000)
+            plt.errorbar(evals[i][0:count], maxCost[i][0:count], yerr=maxCostErr[i][0:count],
+                         label=labels[i], color=colors[i], ecolor=colors[i], errorevery=500)
         elif config == "min":
-            plt.errorbar(evals[i], minCost[i], yerr=maxCostErr[i],
-                         label=labels[i], color=colors[i], ecolor=colors[i], errorevery=1000)
+            plt.errorbar(evals[i][0:count], minCost[i][0:count], yerr=minCostErr[i][0:count],
+                         label=labels[i], color=colors[i], ecolor=colors[i], errorevery=500)
         elif config == "avg":
             plt.plot(evals[i], avgCost[i], label=labels[i])
     plt.legend()
@@ -118,7 +138,7 @@ def plot_learning_curve(args):
     plt.ylabel('Costs')
     plt.xlabel('Log of Evaluations')
     if args.xlimit is not None:
-        plt.xlim(0, int(args.xlimit))
+        plt.xlim(int(args.xlimit[0]), int(args.xlimit[1]))
     plt.show()
 
 
@@ -136,7 +156,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '-t', '--title', nargs='+', help='title of output graph', required=False)
     parser.add_argument(
-        '-xl', '--xlimit', help='truncate x axis', required=False)
+        '-xl', '--xlimit', nargs='+', help='truncate x axis', required=False)
 
     parser.add_argument(
         '-g', '--generation', help='generation to plot path', required=False)
@@ -165,14 +185,18 @@ Compare hill climbers:
  - Shortest: python plotter.py -c min -r 38 39 40 41 42 -r 26 30 31 32 33 -l swap-hill-climber insert-hill-climber -t Shortest_TSP_Solver -lg y
  - Longest: python plotter.py -c min -r 43 44 45 46 47 -r 27 34 35 36 37 -l swap-hill-climber insert-hill-climber -t Longest_TSP_Solver -lg y
 
- Random Search: 
+ Random Search:
  - Shortest: python plotter.py -c min -r 25 56 57 58 59 -l random-search -t Shortest_Random_Search
  - Longest: python plotter.py -c min -r 24 52 53 54 55 -l random-search -t Longest_Random_Search
 
 Multiple Inheritance Insert List
-- Shortest: python plotter.py -c min -r 51 -l insert-multilist-shortest
- Summary:
-  - Shortest: python plotter.py -c min -r 38 39 40 41 42 -r 26 30 31 32 33 -r 25 56 57 58 59 -l swap-hill-climber insert-hill-climber random-search -t TSP_Solver_Comparison -lg y
+- Shortest (full): python plotter.py -c min -r 51 61 64 65 67 -l insert-multiple-inheritance-list -t Evolutionary Algorithm for Shortest TSP
+- Shortest (best): python plotter.py -c min -r 51 67 -l insert-multiple-inheritance-list -t Evolutionary Algorithm for Shortest TSP
+- Longest (full): python plotter.py -c min -r 49 62 63 66 -l insert-multiple-inheritance-list -t Evolutionary Algorithm for Longest TSP
+- Longest (best): python plotter.py -c min -r 49 66 -l insert-multiple-inheritance-list -t Evolutionary Algorithm for Longest TSP
+
+Summary:
+- Shortest: python plotter.py -c min -r 38 39 40 41 42 -r 26 30 31 32 33 -r 25 56 57 58 59 -r 51 67 -l swap-hill-climber insert-hill-climber random-search insert-multiple-inheritance-list -t TSP Solver Comparison -lg y -xl 6 16
 
 Plot Path:
 python plotter.py -c path -r <run-number>